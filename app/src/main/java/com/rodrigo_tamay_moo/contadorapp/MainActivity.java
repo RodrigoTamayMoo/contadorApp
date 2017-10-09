9667e59d9b1ebcc6b346ec758b399c2d811f60f5
RodrigoTamayMoo <14470439@itcampeche.edu.mx>
2017-10-09 12:03:18 -0500

    ContadorApp: Se agrego manejo de eventos a la interfaz de usuario grafica.

--- app/src/main/java/com/rodrigo_tamay_moo/contadorapp/MainActivity.java
+++ app/src/main/java/com/rodrigo_tamay_moo/contadorapp/MainActivity.java
@@ -1,13 +1,38 @@
 package com.rodrigo_tamay_moo.contadorapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBotonContar, mBotonReiniciar;
+    private EditText mTextoContar;
+    int mcontar = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate (Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+
+        mBotonContar = (Button)findViewById(R.id.buton_contar);
+        mBotonReiniciar = (Button)findViewById(R.id.buton_reiniciar);
+        mTextoContar = (EditText) findViewById(R.id.incrementar_valor);
+
+        mBotonContar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcontar++;
+                mTextoContar.setText(mcontar+"");
+
+            }
+        });
     }
+
+
+
+
+
 }
